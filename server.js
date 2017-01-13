dede35a0eae40d177ab3ccad9532006ca83c0a7c
koyexes <no-reply@email.com>
2017-01-13 21:10:10 +0100

    Projects page added

--- server.js
+++ server.js
@@ -53,12 +53,18 @@ app.get('/about',  (request, response) => {
     });
 });
 
+app.get('/projects', (request, response) => {
+    response.render('projects.hbs', {
+       pageTitle: "Projects"
+    });
+});
+
 app.get('/bad', (request, response) => {
 
     response.send({
         errorMessage : 'Unable to serve this request'
     });
-})
+});
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
